2dabd732d8acaada8cf67552751f2cb6ca7e4210
SamanehKeshavarz <samaneh.keshavarz.alizadeh@gmail.com>
2023-11-13 21:33:35 +0330

    new changes for setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your_package_name',
+    name='math_quiz',
     version='0.1',
     packages=find_packages(),
     install_requires=[
