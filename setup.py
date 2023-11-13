b4288b6c7ac8df9584a4236f171a22ff05a23a09
SamanehKeshavarz <samaneh.keshavarz.alizadeh@gmail.com>
2023-11-13 21:18:08 +0330

    add missing files

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='your_package_name',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'dependency1',
+        'dependency2',
+        # Add any other dependencies required for your package
+    ],
+)
