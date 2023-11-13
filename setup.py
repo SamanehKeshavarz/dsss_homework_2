d7fedc0cd245fb20301e4f381f7edf52ac83e14d
SamanehKeshavarz <samaneh.keshavarz.alizadeh@gmail.com>
2023-11-13 21:58:59 +0330

    new changes

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
+from setuptools import setup, find_packages
 
-from setuptools import setup
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
 setup(
-    name='MathTools',
-    version='1.0',
-    packages=['MathTools'],
+    name='example_pkg',
+    version='0.1.0',
+    author='Your Name',
+    author_email='your@email.com',
+    description='A simple example package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/your_username/example_pkg',
+    packages=find_packages(),
     install_requires=[
-        'numpy',
-        'scipy',
+        'requests',  # example dependency
+        'numpy',     # example dependency
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
